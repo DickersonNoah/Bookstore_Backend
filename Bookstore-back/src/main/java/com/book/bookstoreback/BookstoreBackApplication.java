da6fb833ebab86a10a511d18122b2d9fdef7717d
Noah Dickerson <Noah.dickerson00@gmail.com>
2022-02-23 15:31:32 -0500

    first commit

---
+++ Bookstore-back/src/main/java/com/book/bookstoreback/BookstoreBackApplication.java
@@ -0,0 +1,14 @@
+package com.book.bookstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
+
+@SpringBootApplication
+public class BookstoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreBackApplication.class, args);
+    }
+
+}
---
+++ Bookstore-back/src/test/java/com/book/bookstoreback/BookstoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.bookstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
